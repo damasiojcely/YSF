7e708d79710c4965db3a4ceb08762e46ce8fb3d3
KarenOtoya <1411282@utp.edu.pe>
2017-06-17 15:33:36 -0500

    Creacion de clase participantEntity

---
+++ src/pe/com/yoursoccerfield/models/ParticipantEntity.java
@@ -0,0 +1,77 @@
+package pe.com.yoursoccerfield.models;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by otoya on 17/06/2017.
+ */
+
+public class ParticipantEntity extends BaseEntity {
+
+    public ParticipantEntity(Connection connection) {
+        super(connection, "participant");
+    }
+
+    public ParticipantEntity() {
+        super();
+    }
+
+    public List<Participant> findAll(OrganizerEntity organizerEntity) {
+        return findByCriteria("", organizerEntity);
+    }
+
+    public Participant findById(String id,  OrganizerEntity organizerEntity) {
+        String criteria = "participant_id = " + "'" + id + "'";
+        return findByCriteria(criteria, organizerEntity).get(0);
+    }
+
+    public List<Participant> findByCriteria(String criteria, OrganizerEntity organizerEntity) {
+        String sql = getDefaultQuery() + (criteria.isEmpty() ? "" : " WHERE " + criteria);
+        List<Participant> participants = new ArrayList<>();
+        try {
+            ResultSet rs = getConnection().createStatement().executeQuery(sql);
+            if(rs == null) return null;
+            while(rs.next()) participants.add(Participant.build(rs, organizerEntity));
+            return participants;
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return participants;
+    }
+
+    public boolean add(Participant participant) {
+        String sql = "INSERT INTO participant(participant_id, participant_first__name, participant_last_name,organizer_id) VALUES(" +
+                participant.getIdAsValue() + ", " +
+                participant.getFirstName() + ", " +
+                participant.getLastName() + ", " +
+                participant.getOrganizer().getIdAsString() + ")";
+        return change(sql);
+    }
+
+    public boolean update(Participant participant) {
+        String sql = "UPDATE countries SET " +
+                "participant_first_name = " + participant.getFirstName() + ", " +
+               "participant_last_name = "+ participant.getLastName() + ", " +
+                "organizer_id = " + participant.getOrganizer().getIdAsString() +
+                " WHERE participant_id = " + participant.getIdAsValue();
+        return change(sql);
+    }
+
+    public boolean delete(Participant participant) {
+        String sql = "DELETE FROM countries WHERE participant_id = " +
+                participant.getIdAsValue();
+        return change(sql);
+    }
+
+    public boolean delete(String id) {
+        String sql = "DELETE FROM countries WHERE 'participant_id = " +
+                "'" + id + "'";
+        return change(sql);
+    }
+
+
+}
