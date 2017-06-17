0a3461964799ac023d70f46f3de950d97dce4ee9
KarenOtoya <1411282@utp.edu.pe>
2017-06-17 11:35:58 -0500

    Clase participant creada

--- src/pe/com/yoursoccerfield/Participant.java
+++ src/pe/com/yoursoccerfield/Participant.java
@@ -1,20 +1,27 @@
 package pe.com.yoursoccerfield;
 
 /**
- * Created by Pc user on 16/06/2017.
+ * Created by Otoya user on 16/06/2017.
  */
 public class Participant {
     private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
     private String password;
-    private People people;
+    private int Organizer;
+
 
     public Participant() {
     }
 
-    public Participant(int id, String password, People people) {
-        this.id = id;
-        this.password = password;
-        this.people = people;
+    public Participant(int id, String firstName, String lastName, String email, String password, int organizer) {
+        this.setId(id);
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setEmail(email);
+        this.setPassword(password);
+        setOrganizer(organizer);
     }
 
 
@@ -27,6 +34,33 @@ public class Participant {
         return this;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Participant setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Participant setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Participant setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -36,12 +70,12 @@ public class Participant {
         return this;
     }
 
-    public People getPeople() {
-        return people;
+    public int getOrganizer() {
+        return Organizer;
     }
 
-    public Participant setPeople(People people) {
-        this.people = people;
+    public Participant setOrganizer(int organizer) {
+        Organizer = organizer;
         return this;
     }
 }
