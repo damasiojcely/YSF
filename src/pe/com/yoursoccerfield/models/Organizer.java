2d290d44f10e92489df98a12aa1b9ae6ffcb7508
lnakaya1902 <1010123@utp.edu.pe>
2017-06-17 21:27:52 -0500

    aa

--- src/pe/com/yoursoccerfield/models/Organizer.java
+++ src/pe/com/yoursoccerfield/models/Organizer.java
@@ -18,126 +18,101 @@ public class Organizer {
     private String position;
 
     public Organizer(String id, String firstName, String lastName, String email, String password, String dni, String photo, String phone, String position) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.dni = dni;
-        this.photo = photo;
-        this.phone = phone;
-        this.position = position;
+        this.setId(id);
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setEmail(email);
+        this.setPassword(password);
+        this.setDni(dni);
+        this.setPhoto(photo);
+        this.setPhone(phone);
+        this.setPosition(position);
     }
 
     public Organizer() {
 
     }
 
-    public String getId() {
+
+   public String getId() {
         return id;
     }
-    public String getIdAsValue() {
-        return "'" + getId() + "'";}
 
-    public void setId(String id) {
+    public Organizer setId(String id) {
         this.id = id;
+        return this;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public String getFirstNameAsValue(){
-        return "'" + getFirstName() + "'";
-    }
-
-
-    public void setFirstName(String firstName) {
+    public Organizer setFirstName(String firstName) {
         this.firstName = firstName;
+        return this;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public String getLastNameAsValue(){
-        return "'" + getLastName() + "'";
-    }
-
-    public void setLastName(String lastName) {
+    public Organizer setLastName(String lastName) {
         this.lastName = lastName;
+        return this;
     }
 
     public String getEmail() {
         return email;
     }
 
-
-    public void setEmail(String email) {
+    public Organizer setEmail(String email) {
         this.email = email;
+        return this;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public String getpasswordAsValue(){
-        return "'" + getPassword() + "'";
-    }
-
-    public void setPassword(String password) {
+    public Organizer setPassword(String password) {
         this.password = password;
+        return this;
     }
 
     public String getDni() {
         return dni;
     }
 
-    public String getDniAsValue(){
-        return "'" + getDni() + "'";
-    }
-
-
-    public void setDni(String dni) {
+    public Organizer setDni(String dni) {
         this.dni = dni;
+        return this;
     }
 
     public String getPhoto() {
         return photo;
     }
 
-    public String getPhotoAsValue(){
-        return "'" + getPhoto() + "'";
-    }
-
-
-    public void setPhoto(String photo) {
+    public Organizer setPhoto(String photo) {
         this.photo = photo;
+        return this;
     }
 
     public String getPhone() {
         return phone;
     }
 
-    public String getPhoneAsValue(){
-        return "'" + getPhone() + "'";
-    }
-
-    public void setPhone(String phone) {
+    public Organizer setPhone(String phone) {
         this.phone = phone;
+        return this;
     }
 
     public String getPosition() {
         return position;
     }
 
-    public String getPositionAsValue(){
-        return  "'" + getPosition() + "'";
-    }
-
-
-    public void setPosition(String position) {
+    public Organizer setPosition(String position) {
         this.position = position;
+        return this;
     }
 
     public static Organizer build(ResultSet resultSet){
@@ -157,5 +132,4 @@ public class Organizer {
         }
         return null;
     }
-
 }
