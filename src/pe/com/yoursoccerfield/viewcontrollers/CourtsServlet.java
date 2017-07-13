c7e0ecebdeb2c460bf16ee6ebb3a08f252c5bec7
lnakaya1902 <1010123@utp.edu.pe>
2017-07-13 00:41:24 -0500

    Add files via upload
    Insertando viewcontrollers

---
+++ src/pe/com/yoursoccerfield/viewcontrollers/CourtsServlet.java
@@ -0,0 +1,94 @@
+package pe.com.yoursoccerfield.viewcontrollers;
+
+import pe.com.yoursoccerfield.models.Court;
+import pe.com.yoursoccerfield.services.YSFService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CourtsServlet" , urlPatterns = "/courts")
+public class CourtsServlet extends HttpServlet {
+
+    YSFService service = new YSFService();
+
+    public static String COURTS_EDIT_URI = "/editCourt.jsp";
+    public static String COURTS_ADD_URI = "/newCourt.jsp";
+    public static String COURTS_INDEX_URI = "/listCourts.jsp";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+        String action = request.getParameter("action");
+        switch (action){
+            case "update": {
+                Court court = service.getCourtById(request.getParameter("id"));
+                court.setName(request.getParameter("name"));
+                court.setCapacity(Integer.parseInt(request.getParameter("capacity")));
+                court.setAddress(request.getParameter("address"));
+                court.setEmail(request.getParameter("email"));
+                court.setPhone(request.getParameter("phone"));
+                court.setPrice(Float.parseFloat(request.getParameter("price")));
+                court.setPhoto(request.getParameter("photo"));
+                court.setType(Integer.parseInt(request.getParameter("type")));
+                court.setState(Boolean.parseBoolean(request.getParameter("state")));
+                String message = service.updateCourt(court) ?
+                        "Update success" :
+                        "Error while updating";
+                log(message);
+            }
+            case "agregate":{
+                Court court= new Court();
+                court.setId(request.getParameter("id"));
+                court.setName(request.getParameter("name"));
+                court.setCapacity(Integer.parseInt(request.getParameter("capacity")));
+                court.setAddress(request.getParameter("address"));
+                court.setEmail(request.getParameter("email"));
+                court.setPhone(request.getParameter("phone"));
+                court.setPrice(Float.parseFloat(request.getParameter("price")));
+                court.setPhoto(request.getParameter("photo"));
+                court.setType(Integer.parseInt(request.getParameter("type")));
+                court.setState(Boolean.parseBoolean(request.getParameter("state")));
+                String message = service.addCourt(court) ?
+                        "Update success" :
+                        "Error while updating";
+                log(message);
+            }
+            case "delete":{
+                Court court = service.getCourtById(request.getParameter("id"));
+                String message = service.deleteCourt(court) ?
+                        "Delete success" :
+                        "Error while delet";
+                log(message);
+            }
+
+        }
+        RequestDispatcher dispatcher = request.getRequestDispatcher(COURTS_INDEX_URI);
+        dispatcher.forward(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+      String action = request.getParameter("action");
+      String actionUri;
+      switch (action){
+          case "add":{
+              actionUri = COURTS_ADD_URI;
+              request.setAttribute("action","add");
+              break;
+          }
+          case "edit":{
+              Court court = service.getCourtById(request.getParameter("id"));
+              request.setAttribute("court",court);
+              request.setAttribute("action","edit");
+              actionUri = COURTS_EDIT_URI;
+              break;
+          }
+          default:
+              actionUri = COURTS_INDEX_URI;
+      }
+      RequestDispatcher dispatcher = request.getRequestDispatcher(actionUri);
+      dispatcher.forward(request,response);
+    }
+}
---
+++ src/pe/com/yoursoccerfield/viewcontrollers/ReservationsServlet.java
@@ -0,0 +1,86 @@
+package pe.com.yoursoccerfield.viewcontrollers;
+
+import pe.com.yoursoccerfield.models.Reservation;
+import pe.com.yoursoccerfield.services.YSFService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Date;
+
+@WebServlet(name = "ReservationsServlet", urlPatterns = "/reservations")
+public class ReservationsServlet extends HttpServlet {
+
+    YSFService service = new YSFService();
+
+    public static String RESERVATIONS_EDIT_URI = "/editReservation.jsp";
+    public static String RESERVATIONS_ADD_URI = "/newReservation.jsp";
+    public static String RESERVATIONS_INDEX_URI = "/listReservations.jsp";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+       String action = request.getParameter("action");
+       switch (action){
+           case "update": {
+               Reservation reservation = service.getReservationById(request.getParameter("id"));
+               reservation.setCreateDate(Date.valueOf(request.getParameter("createDate")));
+               reservation.setState(Boolean.valueOf(request.getParameter("state")));
+               reservation.setGameTime(Date.valueOf(request.getParameter("gameTime")));
+               reservation.setHours(Float.parseFloat(request.getParameter("hours")));
+               reservation.setTotal(Float.parseFloat(request.getParameter("total")));
+               String message = service.updateReservation(reservation)?
+                       "Update sucess" :
+                       "Error while updating";
+               log(message);
+           }
+           case "agregate":{
+               Reservation reservation=new Reservation();
+               reservation.setId(Integer.parseInt(request.getParameter("id")));
+               reservation.setCreateDate(Date.valueOf(request.getParameter("createDate")));
+               reservation.setState(Boolean.valueOf(request.getParameter("state")));
+               reservation.setGameTime(Date.valueOf(request.getParameter("gameTime")));
+               reservation.setHours(Float.parseFloat(request.getParameter("hours")));
+               reservation.setTotal(Float.parseFloat(request.getParameter("total")));
+               String message = service.addReservation(reservation)?
+                       "Update sucess" :
+                       "Error while updating";
+               log(message);
+           }
+           case "delete":{
+               Reservation reservation=service.getReservationById(request.getParameter("id"));
+               String message=service.deleteReservation(reservation) ?
+                       "Delete success" :
+                       "Error while del";
+               log(message);
+           }
+       }
+        RequestDispatcher dispatcher=request.getRequestDispatcher(RESERVATIONS_INDEX_URI);
+       dispatcher.forward(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException ,IOException{
+        String action = request.getParameter("action");
+        String actionUri;
+        switch (action){
+            case "add": {
+                actionUri = RESERVATIONS_ADD_URI;
+                request.setAttribute("action","add");
+                break;
+            }
+            case "edit": {
+                Reservation reservation = service.getReservationById(request.getParameter("id"));
+                request.setAttribute("reservation",reservation);
+                request.setAttribute("action","edit");
+                actionUri = RESERVATIONS_EDIT_URI;
+                break;
+            }
+            default:
+                actionUri=RESERVATIONS_INDEX_URI;
+        }
+        RequestDispatcher dispatcher=request.getRequestDispatcher(actionUri);
+        dispatcher.forward(request,response);
+    }
+}
